559f985b9f060eb805d866b73c3fd89ce5b62b3a
Ben Zimmer <ben.d.zimmer@gmail.com>
2015-09-12 15:49:17 -0500

    fixed launch scripts

--- dist/secondary.bat
+++ dist/secondary.bat
@@ -2,4 +2,4 @@
 rem Copyright (c) 2015 Ben Zimmer. All rights reserved.
 rem Batch file for running Secondary on Windows.
 
-java -cp secondary.jar bdzimmer.secondary.export.Driver
+java -cp %~dp0\secondary.jar bdzimmer.secondary.export.Driver %*
