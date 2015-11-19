c5dc3c2a1c6f093ef37a73d4cf46d66cd06cccf9
Ben Zimmer <ben.d.zimmer@gmail.com>
2015-11-18 23:31:28 -0600

    initial tag parsing test suite

---
+++ src/test/scala/bdzimmer/secondary/export/model/ParseSecTagsSuite.scala
@@ -0,0 +1,27 @@
+// Copyright (c) 2015 Ben Zimmer. All rights reserved.
+
+// Tests for ParseSecTags.
+
+package bdzimmer.secondary.export.model
+
+import org.scalatest.FunSuite
+
+class ParseSecTagsSuite extends FunSuite {
+
+  test("tag parsing") {
+
+    // if no kind is given, default to link
+    assert(ParseSecTags.getTag("Default Link")
+        .equals(SecTag("link", "Default Link", List())))
+
+    // kind given, no parameters
+    assert(ParseSecTags.getTag("familytree: Billy Bob")
+        .equals(SecTag("familytree", "Billy Bob", List())))
+
+    // kind with parameters
+    assert(ParseSecTags.getTag("jumbotron: Baloney Photo | color=white xpos=0% ypos=50%")
+        .equals(SecTag("jumbotron", "Baloney Photo", List("color=white", "xpos=0%", "ypos=50%"))))
+
+  }
+
+}
