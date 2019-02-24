c185432d86961851b9a467ff71c85917c9ee42c7
Ben Zimmer <ben.d.zimmer@gmail.com>
2019-02-23 20:42:45 -0600

    WIP duplicate word detection

---
+++ src/main/scala/bdzimmer/secondary/export/controller/Dup.scala
@@ -0,0 +1,17 @@
+// Copyright (c) 2019 Ben Zimmer. All rights reserved.
+// Duplicate word detection.
+
+package bdzimmer.secondary.export.controller
+
+
+object Dup {
+
+  // val Matcher = "(\\b\\w+\\b)(\\s+\\1)+".r
+  val Matcher = "(\\b\\S+)(?:\\s+\\1\\b)+".r
+
+  def find(notes: String): List[(Int, Int)] = {
+    notes.split("\n").toList.zipWithIndex.flatMap(
+      x => Matcher.findAllMatchIn(x._1).map(m => (x._2, m.start)))
+  }
+
+}
---
+++ src/test/scala/bdzimmer/secondary/export/controller/DupSuite.scala
@@ -0,0 +1,18 @@
+// Copyright (c) 2019 Ben Zimmer. All rights reserved.
+
+// Tests for duplicate word detection.
+
+package bdzimmer.secondary.export.controller
+
+import org.scalatest.FunSuite
+
+
+class DupSuite extends FunSuite{
+
+  test("duplicate") {
+    val testLines = "the quick quick brown\nfox jumps over\nthe the lazy dog dog\nThat's something.."
+    val lineNumbers = Dup.find(testLines)
+    assert(lineNumbers == List((0, 4), (2, 0), (2, 13)))
+  }
+
+}
