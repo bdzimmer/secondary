c187c20d488f0c53c03800d3680e875d9b40511d
Ben Zimmer <ben.d.zimmer@gmail.com>
2019-07-28 19:21:02 -0500

    WIP basic output

---
+++ src/main/scala/bdzimmer/secondary/export/controller/Basic.scala
@@ -0,0 +1,19 @@
+// Copyright (c) 2019 Ben Zimmer. All rights reserved.
+
+// Prepare minimally-formated HTML documents suitable for automated conversion to doc / docx.
+
+package bdzimmer.secondary.export.controller
+
+
+object Basic {
+
+
+
+    def convert(): String = {
+
+      // TODO: additional formatting
+
+      ""
+    }
+
+}
