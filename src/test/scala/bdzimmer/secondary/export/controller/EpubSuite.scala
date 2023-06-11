1b3a96237fff268e6e4ee5912da622bde1b1adf7
Ben Zimmer <ben.d.zimmer@gmail.com>
2023-06-11 17:42:35 -0500

    cleanup

--- src/test/scala/bdzimmer/secondary/export/controller/EpubSuite.scala
+++ src/test/scala/bdzimmer/secondary/export/controller/EpubSuite.scala
@@ -22,10 +22,10 @@ class EpubSuite extends FunSuite with TempDirectory {
        outputFilename,
        "0000",
        "Test Book",
-       Some(("D", "R")),
-       Some("K R"),
+       Some(("B", "Z")),
+       Some("C Z"),
        List(
-         Book.SectionInfo("0", "Title Page", None, None, "<body>Test Book<br>B Z</body>"),
+         Book.SectionInfo("0", "Title Page", None, None, "<body>Test Book<br>D Z</body>"),
          Book.SectionInfo("1", "Chapter 1", None, None, "<body>This is the content of chapter 1.</body>")
        ),
        None,
